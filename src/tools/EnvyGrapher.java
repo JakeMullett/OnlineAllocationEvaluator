28ca0bf8a2dcfdf98609d5bf347727b51f50acbe
Jacob Mullett <jtm51@duke.edu>
2018-12-11 00:23:31 -0500

    grapher update

--- src/tools/EnvyGrapher.java
+++ src/tools/EnvyGrapher.java
@@ -22,18 +22,28 @@ public class EnvyGrapher {
         Map<Person, List<Task>> allocations = group.getPersonTasksMap();
         Person[] people = getPeople(allocations);
         double[][] zValues = MetricsCalculator.calculateEnvyGraph(group);
-        HeatChart hc = new HeatChart(zValues, -20, 20);
+        removeNegatives(zValues);
+        HeatChart hc = new HeatChart(zValues, 0, 20);
         hc.setXValues(people);
         hc.setYValues(people);
+        hc.setChartMargin(50);
+        hc.setBackgroundColour(new Color(255,255,255,255));
         hc.setHighValueColour(Color.RED);
-        hc.setLowValueColour(Color.GREEN);
-        hc.setTitle("Pairwise Envy of Tasks Allocated"); // probably make more verbose soon
+        hc.setLowValueColour(Color.lightGray);
+        hc.setTitle(algorithmName); // probably make more verbose soon
         try {
             hc.saveToFile(fileDest);
         } catch (IOException ex) {
             System.out.println(fileName + " could not be created.");
             ex.printStackTrace();
         }
+
+    }
+
+    public static void removeNegatives(double[][] vals) {
+        for (int i = 0; i< vals.length; i++)
+            for (int j = 0; j < vals.length; j++)
+                vals[i][j] = Math.max(0.0, vals[i][j]);
 
     }
 }
